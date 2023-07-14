47cc7f5cdad3352dbb43680651df84ecba55ad2c
rpaskalev <rpaskalev@lomixtech.com>
2023-07-14 19:20:09 -0400

    harden vpc and ec2 code

--- ec2.tf
+++ ec2.tf
@@ -1,20 +1,50 @@
 resource "aws_instance" "ziyote_ec2_first" {
   ami           = "ami-06b09bfacae1453cb" #works ONLY in us-east-1
-  instance_type = "t2.micro"
+  instance_type = "t3.micro" #t2.micro does not support ebs opt/monitoring
 
   subnet_id = aws_subnet.public_subnet_1.id
   key_name  = "radostinpaskalev_key"
   # we will be using the heredoc syntax
-  user_data                   = <<EOF
-  #!/bin/bash
-  sudo yum update -y
-  sudo yum install -y httpd
-  sudo systemctl start httpd.service
-  sudo systemctl enable httpd.service
-  sudo echo "<h1> At $(hostname -f) </h1>" > /var/www/html/index.html
-  EOF
+  vpc_security_group_ids = [
+    aws_security_group.ziyo_allow_https.id
+    ]
+  
+  # user_data                   = <<EOF
+  # #!/bin/bash
+  # sudo yum update -y
+  # sudo yum install -y httpd
+  # sudo systemctl start httpd.service
+  # sudo systemctl enable httpd.service
+  # sudo echo "</h1> At $(hostname -f) </h1>" > /var/www/html/index.html
+  # EOF
+  user_data                   = file("userdata.sh")
   associate_public_ip_address = true
+  ebs_optimized               = true
+  monitoring                  = true
   tags = {
     Name = "Ziyo_first_instance"
+  }
+}
+
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow ALL inbound traffic"
+  depends_on  = [aws_vpc.ziyo_vpc]
+  vpc_id      = aws_vpc.ziyo_vpc.id
+
+  ingress {
+    description = "All from VPC"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
--- vpc.tf
+++ vpc.tf
@@ -7,20 +7,39 @@ resource "aws_vpc" "ziyo_vpc" {
 }
 
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id     = aws_vpc.ziyo_vpc.id
-  cidr_block = "10.10.1.0/24"
-
+  vpc_id            = aws_vpc.ziyo_vpc.id
+  cidr_block        = "10.10.1.0/24"
+  availability_zone = "us-east-1a"
   tags = {
     Name = "public_subnet_1"
   }
 }
 
-resource "aws_subnet" "private_subnet_1" {
-  vpc_id     = aws_vpc.ziyo_vpc.id
-  cidr_block = "10.10.2.0/24"
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.ziyo_vpc.id
+  cidr_block        = "10.10.2.0/24"
+  availability_zone = "us-east-1b"
 
   tags = {
+    Name = "public_subnet_2"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.ziyo_vpc.id
+  cidr_block        = "10.10.3.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
     Name = "private_subnet_1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.ziyo_vpc.id
+  cidr_block        = "10.10.4.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "private_subnet_2"
   }
 }
 
@@ -45,9 +64,26 @@ resource "aws_route_table" "ziyo_route_table" {
   }
 }
 
+resource "aws_route_table" "ziyo_route_table_2" {
+  vpc_id = aws_vpc.ziyo_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ziyo_igw.id
+  }
+
+  tags = {
+    Name = "ziyo_route_table_2"
+  }
+}
+
 resource "aws_route_table_association" "ziyo_route_assoc" {
   subnet_id      = aws_subnet.public_subnet_1.id
   route_table_id = aws_route_table.ziyo_route_table.id
 }
 
+resource "aws_route_table_association" "ziyo_route_assoc_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.ziyo_route_table_2.id
+}
  
