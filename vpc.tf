69b383400414b64b2408c5af8c7e27a68f7f6177
BorisUrten <136039813+BorisUrten@users.noreply.github.com>
2023-08-02 20:45:12 -0400

    Update vpc.tf
    changed the vpc.tf file

--- vpc.tf
+++ vpc.tf
@@ -1,90 +1,130 @@
-resource "aws_vpc" "ziyo_vpc" {
-  cidr_block = "10.10.0.0/16"
+provider "aws" {
+  region = var.aws_region
+}
 
-  tags = {
-    Name = "Ziyo_2023_VPC"
-  }
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr_block
+  tags       = var.public_subnet_1_tags
 }
 
 
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id            = aws_vpc.ziyo_vpc.id
-  cidr_block        = "10.10.1.0/24"
-  availability_zone = "us-east-1a"
-  tags = {
-    Name = "public_subnet_1"
-  }
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_1_cidr
+  availability_zone = var.public_subnet_1_az
+  tags       = var.public_subnet_1_tags
 }
+
 
 resource "aws_subnet" "public_subnet_2" {
-  vpc_id            = aws_vpc.ziyo_vpc.id
-  cidr_block        = "10.10.2.0/24"
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "public_subnet_2"
-  }
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_2_cidr
+  availability_zone = var.public_subnet_2_az
+  tags       = var.public_subnet_2_tags
 }
+
 
 resource "aws_subnet" "private_subnet_1" {
-  vpc_id            = aws_vpc.ziyo_vpc.id
-  cidr_block        = "10.10.3.0/24"
-  availability_zone = "us-east-1a"
-  tags = {
-    Name = "private_subnet_1"
-  }
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_1_cidr
+  availability_zone = var.private_subnet_1_az
+  tags       = var.private_subnet_1_tags
 }
+
 
 resource "aws_subnet" "private_subnet_2" {
-  vpc_id            = aws_vpc.ziyo_vpc.id
-  cidr_block        = "10.10.4.0/24"
-  availability_zone = "us-east-1b"
-  tags = {
-    Name = "private_subnet_2"
-  }
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_2_cidr
+  availability_zone = var.private_subnet_2_az
+  tags       = var.private_subnet_2_tags
 }
 
-resource "aws_internet_gateway" "ziyo_igw" {
-  vpc_id = aws_vpc.ziyo_vpc.id
 
-  tags = {
-    Name = "ziyo_internet_gateway"
-  }
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags   = var.internet_gateway_tags
 }
 
-resource "aws_route_table" "ziyo_route_table" {
-  vpc_id = aws_vpc.ziyo_vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.ziyo_igw.id
-  }
-
-  tags = {
-    Name = "ziyo_route_table"
-  }
+resource "aws_route_table" "public_subnet_1_route_table" {
+  vpc_id = aws_vpc.main.id
+  tags   = var.route_table_tags
 }
 
-resource "aws_route_table" "ziyo_route_table_2" {
-  vpc_id = aws_vpc.ziyo_vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.ziyo_igw.id
-  }
-
-  tags = {
-    Name = "ziyo_route_table_2"
-  }
+resource "aws_route_table" "public_subnet_2_route_table" {
+  vpc_id = aws_vpc.main.id
+  tags   = var.route_table_2_tags
 }
 
-resource "aws_route_table_association" "ziyo_route_assoc" {
+
+resource "aws_route_table_association" "public_subnet_1_association" {
   subnet_id      = aws_subnet.public_subnet_1.id
-  route_table_id = aws_route_table.ziyo_route_table.id
+  route_table_id = aws_route_table.public_subnet_1_route_table.id
 }
 
-resource "aws_route_table_association" "ziyo_route_assoc_2" {
+
+resource "aws_route_table_association" "public_subnet_2_association" {
   subnet_id      = aws_subnet.public_subnet_2.id
-  route_table_id = aws_route_table.ziyo_route_table_2.id
+  route_table_id = aws_route_table.public_subnet_2_route_table.id
+}
+
+
+resource "aws_security_group" "instance_sg" {
+  name_prefix = "instance_sg_"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "Allow SSH inbound traffic"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP inbound traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_instance" "example_instance" {
+  ami           = var.instance_ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  subnet_id     = aws_subnet.public_subnet_1.id
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
+  user_data     = file(var.userdata_file_path)
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
+
+
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = var.bucket_name
+  acl    = "private"
+  force_destroy = var.force_destroy
+
+  object_lock_configuration {
+    object_lock_enabled = var.object_lock_enabled
+  }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.example_instance.public_ip
 }
  
